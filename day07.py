d28fdb1322ff97f9add166a24fa41f7a0ffd0fd2
shafe123 <ethan.h.shafer@gmail.com>
2025-12-07 13:27:32 -0500

    day 07 part 2 att 1, going to time out...

--- day07.py
+++ day07.py
@@ -31,6 +31,82 @@ def part1(testing=True):
     return split_count
 
 
+@separator()
+def part2_att1(testing=True):
+    data: list[list[str]] = get_lines(7, testing, split_str=True)  # type: ignore
+
+    start_col = data[0].index("S")
+    # use a LIST to keep track of which columns are there
+    current_row_beams = [start_col]
+    split_count = 0
+
+    for row_num, row in enumerate(data):
+        if row_num == 0:
+            continue
+
+        previous_row_beams = current_row_beams.copy()
+        current_row_beams = []
+
+        # go by beam instead of by column (which I should've done above)
+        num_beams = len(previous_row_beams)
+        beam_index = 0
+        while beam_index < num_beams:
+            # propagate the tachyon further down
+            col_index = previous_row_beams[beam_index]
+            if row[col_index] != "^":
+                current_row_beams.append(col_index)
+                # only increment here when we don't delete the beam
+                beam_index += 1
+
+            # split the beam (and remove the current beam)
+            elif row[col_index] == "^":
+                current_row_beams.append(col_index - 1)
+                current_row_beams.append(col_index + 1)
+
+                del previous_row_beams[beam_index]
+                num_beams -= 1
+
+    return current_row_beams
+
+
+@separator()
+def part2_att1(testing=True):
+    data: list[list[str]] = get_lines(7, testing, split_str=True)  # type: ignore
+
+    start_col = data[0].index("S")
+    # use a LIST to keep track of which columns are there
+    current_row_beams = [start_col]
+    split_count = 0
+
+    for row_num, row in enumerate(data):
+        if row_num == 0:
+            continue
+
+        previous_row_beams = current_row_beams.copy()
+        current_row_beams = []
+
+        # go by beam instead of by column (which I should've done above)
+        num_beams = len(previous_row_beams)
+        beam_index = 0
+        while beam_index < num_beams:
+            # propagate the tachyon further down
+            col_index = previous_row_beams[beam_index]
+            if row[col_index] != "^":
+                current_row_beams.append(col_index)
+                # only increment here when we don't delete the beam
+                beam_index += 1
+
+            # split the beam (and remove the current beam)
+            elif row[col_index] == "^":
+                current_row_beams.append(col_index - 1)
+                current_row_beams.append(col_index + 1)
+
+                del previous_row_beams[beam_index]
+                num_beams -= 1
+
+    return current_row_beams
+
+
 if __name__ == "__main__":
-    result = part1(False)
-    print(result)
+    result = part2_att1(False)
+    print(len(result))
